4b07af226d64232b76f4c42435befc8b03c88b1d
Algirdas <algirdas@mediapark.lt>
2015-10-14 23:30:25 +0300

    Canvas fractal animations of transforms add

--- coffee/CanvasFractal.coffee
+++ coffee/CanvasFractal.coffee
@@ -1,91 +1,266 @@
+class RotationTransformation
+	constructor: (@current, @max, @increaseBy = 1, @invert = false) ->
+
+	increase: (howMuch = @increaseBy || 1) ->
+		@current += howMuch
+
+	fullyEnded: ->
+		@ended()
+
+	ended: ->
+		if !@invert then @current >= @max else @current <= @max
+
+	increaseIfNotEnded: ->
+		if !@ended()
+			@increase()
+
+	rotate: ->
+		MathUtils.toRadians @current
+
+class Transformation 
+	@TYPES:
+		Y: 'y'
+		X: 'x'
+	@INCREASE_TYPES:
+		X: Transformation.X
+		Y: Transformation.Y
+		BOTH: 'both'
+	constructor: (@current = {x: 0, y: 0}, @max = {x: 0, y: 0}, @increaseBy = {x: 1, y: 1}, @invert = {x: false, y: false}) ->
+
+	increase: (which, howMuch) ->
+		howMuch = @increaseBy[which] || 1
+		@current[which] += howMuch
+
+	fullyEnded: ->
+		@ended(Transformation.TYPES.Y) and @ended(Transformation.TYPES.X)
+
+	partlyEnded: ->
+		@ended(Transformation.TYPES.Y) or @ended(Transformation.TYPES.X)
+
+	ended: (which) ->
+		if !@invert[which] then @current[which] >= @max[which] else @current[which] <= @max[which]
+
+	increaseIfNotEnded: (which = Transformation.INCREASE_TYPES.BOTH) ->
+		switch which
+			when Transformation.INCREASE_TYPES.Y
+				if !@ended Transformation.TYPES.Y
+					@increase Transformation.TYPES.Y
+			when Transformation.INCREASE_TYPES.X
+				if !@ended Transformation.TYPES.X
+					@increase Transformation.TYPES.X
+			when Transformation.INCREASE_TYPES.BOTH
+				if !@ended Transformation.TYPES.Y
+					@increase Transformation.TYPES.Y
+				if !@ended Transformation.TYPES.X
+					@increase Transformation.TYPES.X
+
+	x: ->
+		@current.x
+	y: ->
+		@current.y
+
+class Transformations
+	constructor: (@t = {translate: null, rotate: null, scale: null}, callbacks = {increase: null, ended: null}) ->
+		@translate = @t.translate
+		@rotate = @t.rotate
+		@scale = @t.scale
+		@callbacks =
+			ended: callbacks.ended || ->
+				return false
+			increase: callbacks.increase || ->
+				return false
+
+	increase: ->
+		@callbacks.increase()
+
+	ended: ->
+		@callbacks.ended()
+
 class CanvasFractal
-  @RECURISE: 'recursive'
-  @ONCE: 'once'
-  @MAXIMUM_STEPS: 7
-  @DEFAULT_STEP: 1
-  constructor: ->
-    @canvas = document.getElementById 'canvas'
-    @ctx = null
-    if canvas.getContext
-      @ctx = @canvas.getContext '2d'
+	@RECURISE: 'recursive'
+	@ONCE: 'once'
+	@MAXIMUM_STEPS: 7
+	@DEFAULT_STEP: 0
+	constructor: ->
+		@canvas = document.getElementById 'canvas'
+		@ctx = null
+		if canvas.getContext
+			@ctx = @canvas.getContext '2d'
 
-    @initClickListener()
-    @currentStep = CanvasFractal.DEFAULT_STEP
-    @firstTime = false
+		@initClickListener()
+		@currentStep = CanvasFractal.DEFAULT_STEP
 
-  setupContext: ->
-    @ctx.beginPath()
-    @ctx.moveTo 0, 0
-    @ctx.lineTo 500, 0
-    @ctx.lineTo 500, 500
-    @ctx.lineTo 0, 500
-    @ctx.closePath()
-    @ctx.stroke(); 
+	setupContext: ->
+		@ctx.beginPath()
+		@ctx.moveTo 0, 0
+		@ctx.lineTo 500, 0
+		@ctx.lineTo 500, 500
+		@ctx.lineTo 0, 500
+		@ctx.closePath()
+		@ctx.stroke(); 
 
-  draw: ->
-    @setupContext()
-    @startDrawing 0, CanvasFractal.RECURSIVE
+		@_savedStep = @currentStep
 
-  initClickListener: ->
-    $(window).on 'keydown', (ev) =>
-      if ev.which == 32
-        @increaseCurrentStep()
-        @ctx.setTransform 1, 0, 0, 1, 0, 0
-        @ctx.clearRect 0, 0, @canvas.width, @canvas.height
-        @startDrawing @currentStep
-        @firstTime = true
+	draw: ->
+		@setupContext()
+		@startDrawing 0, CanvasFractal.RECURSIVE
 
-  clear: ->
+	initClickListener: ->
+		$(window).on 'keydown', (ev) =>
+			switch ev.which 
+				when 32
+					@increaseCurrentStep()
+					@clear()
+					@startDrawing @currentStep
+				when 49
+					@clear()
+					transformations = new Transformations(
+						{
+							rotate: null, 
+							translate: new Transformation({x: 0, y: 0}, {x: 250, y: 250}), 
+							scale: new Transformation({x: 1, y: 1}, {x: 0.5, y: 0.5}, {x: -0.005, y: -0.005}, {x: true, y: true})
+						},
+						{
+							increase: ->
+								transformations.scale.increaseIfNotEnded Transformation.INCREASE_TYPES.BOTH
+								transformations.translate.increaseIfNotEnded Transformation.INCREASE_TYPES.BOTH
+							ended: ->
+								return transformations.scale.fullyEnded() and transformations.translate.fullyEnded()
+						}	
+					)
+					@doAnimation transformations
+				when 50
+					@clear()
+					transformations = new Transformations(
+						{
+							rotate: new RotationTransformation(0, -90, -1, true),
+							translate: new Transformation({x: 0, y: 0}, {x: 250, y: 0}),
+							scale: new Transformation({x: 1, y: 1}, {x: -0.5, y: 0.5}, {x: -0.005, y: -0.005}, {x: true, y: true})
+						},
+						{
+							increase: ->
+								transformations.scale.increaseIfNotEnded Transformation.INCREASE_TYPES.BOTH
+								transformations.translate.increaseIfNotEnded Transformation.INCREASE_TYPES.BOTH
+								transformations.rotate.increaseIfNotEnded()
+							ended: ->
+								return transformations.scale.fullyEnded() and transformations.translate.fullyEnded() and transformations.rotate.fullyEnded()
+						}	
+					)
+					@doAnimation transformations
+				when 51
+					@clear()
+					transformations = new Transformations(
+						{
+							translate: new Transformation({x: 0, y: 0}, {x: 250, y: 0}),
+							scale: new Transformation({x: 1, y: 1}, {x: -0.5, y: 0.5}, {x: -0.005, y: -0.005}, {x: true, y: true})
+						},
+						{
+							increase: ->
+								transformations.scale.increaseIfNotEnded Transformation.INCREASE_TYPES.BOTH
+								transformations.translate.increaseIfNotEnded Transformation.INCREASE_TYPES.BOTH
+							ended: ->
+								return transformations.scale.fullyEnded() and transformations.translate.fullyEnded()
+						}	
+					)
+					@doAnimation transformations
+					
+				when 52
+					@clear()
+					transformations = new Transformations(
+						{
+							rotate: new RotationTransformation(0, 90, 1),
+							translate: new Transformation({x: 0, y: 0}, {x: 125, y: 250}),
+							scale: new Transformation({x: 1, y: 1}, {x: 0.25, y: 0.25}, {x: -0.005, y: -0.005}, {x: true, y: true})
+						},
+						{
+							increase: ->
+								transformations.scale.increaseIfNotEnded Transformation.INCREASE_TYPES.BOTH
+								transformations.translate.increaseIfNotEnded Transformation.INCREASE_TYPES.BOTH
+								transformations.rotate.increaseIfNotEnded()
+							ended: ->
+								return transformations.scale.fullyEnded() and transformations.translate.fullyEnded() and transformations.rotate.fullyEnded()
+						}	
+					)
+					@doAnimation transformations
 
+	clear: (preserveTransform = false) ->
+		if preserveTransform
+			@ctx.save()
+			@ctx.setTransform 1, 0, 0, 1, 0, 0
 
-  increaseCurrentStep: ->
-    if @currentStep >= CanvasFractal.MAXIMUM_STEPS
-      @currentStep = CanvasFractal.DEFAULT_STEP
-    else
-      @currentStep++
+		@ctx.clearRect 0, 0, @canvas.width, @canvas.height
 
-  startDrawing: (currentStep, how = CanvasFractal.RECURSIVE) ->
-    switch how
-      when CanvasFractal.RECURSIVE
-        if currentStep > 0 
-          currentStep--
-          @ctx.save()
-          @ctx.save()
-          @ctx.save()
-          @ctx.translate 250, 250
-          @ctx.scale 0.5, 0.5
-          @ctx.fillStyle = 'green'
-          @startDrawing currentStep
-          @ctx.restore()
-          @ctx.translate 250, 0
-          @ctx.rotate MathUtils.toRadians -90
-          @ctx.scale -0.5, 0.5
-          @ctx.fillStyle = 'yellow'
-          @startDrawing currentStep
-          @ctx.restore()
-          @ctx.translate 250, 0
-          @ctx.scale -0.5, 0.5
-          @ctx.fillStyle = 'red'
-          @startDrawing currentStep
-          @ctx.restore()
-          @ctx.translate 125, 250
-          @ctx.rotate MathUtils.toRadians 90
-          @ctx.scale 0.25, 0.25
-          @ctx.fillStyle = 'blue'
-          @startDrawing currentStep
-        else @startDrawing 0, CanvasFractal.ONCE
-      when CanvasFractal.ONCE
-        @ctx.beginPath()
-        @ctx.moveTo 75, 0
-        @ctx.lineTo 425, 0
-        @ctx.lineTo 500, 250
-        @ctx.lineTo 500, 500
-        @ctx.lineTo 0, 375
-        @ctx.lineTo 0, 75
-        #@ctx.fillStyle = "blue"
-        @ctx.fill()
+		if preserveTransform
+			@ctx.restore()
+
+	increaseCurrentStep: ->
+		if @currentStep >= CanvasFractal.MAXIMUM_STEPS
+			@currentStep = CanvasFractal.DEFAULT_STEP
+		else
+			@currentStep++
+
+	doAnimation: (transformations) ->
+		interval = setInterval =>
+			transformations.increase()
+
+			if transformations.ended()
+				clearInterval interval
+
+			@clear false
+
+			@ctx.save()
+			@ctx.translate transformations.translate.x(), transformations.translate.y() if transformations.translate?
+			@ctx.rotate transformations.rotate.rotate() if transformations.rotate?
+			@ctx.scale transformations.scale.x(), transformations.scale.y() if transformations.scale?
+			@ctx.fillStyle = 'red'
+			@startDrawing 0, CanvasFractal.ONCE
+			@ctx.restore()
+
+		, 5
+
+	startDrawing: (currentStep, how = CanvasFractal.RECURSIVE) ->
+		switch how
+			when CanvasFractal.RECURSIVE
+				if currentStep > 0 
+					currentStep--
+					@ctx.save()
+					@ctx.save()
+					@ctx.save()
+					@ctx.save()
+					@ctx.translate 250, 250
+					@ctx.scale 0.5, 0.5
+					@ctx.fillStyle = 'green'
+					@startDrawing currentStep
+					@ctx.restore()
+					@ctx.translate 250, 0
+					@ctx.rotate MathUtils.toRadians -90
+					@ctx.scale -0.5, 0.5
+					@ctx.fillStyle = 'yellow'
+					@startDrawing currentStep
+					@ctx.restore()
+					@ctx.translate 250, 0
+					@ctx.scale -0.5, 0.5
+					@ctx.fillStyle = 'red'
+					@startDrawing currentStep
+					@ctx.restore()
+					@ctx.translate 125, 250
+					@ctx.rotate MathUtils.toRadians 90
+					@ctx.scale 0.25, 0.25
+					@ctx.fillStyle = 'blue'
+					@startDrawing currentStep
+					@ctx.restore()
+				else @startDrawing 0, CanvasFractal.ONCE
+			when CanvasFractal.ONCE
+				@ctx.beginPath()
+				@ctx.moveTo 75, 0
+				@ctx.lineTo 425, 0
+				@ctx.lineTo 500, 250
+				@ctx.lineTo 500, 500
+				@ctx.lineTo 0, 375
+				@ctx.lineTo 0, 75
+				@ctx.fill()
 
 
 $(document).ready ->
-  @canvasFractal = new CanvasFractal()
-  @canvasFractal.draw()
+	@canvasFractal = new CanvasFractal()
+	@canvasFractal.draw()
--- js/CanvasFractal.js
+++ js/CanvasFractal.js
@@ -1,4 +1,172 @@
-var CanvasFractal;
+var CanvasFractal, RotationTransformation, Transformation, Transformations;
+
+RotationTransformation = (function() {
+  function RotationTransformation(current, max, increaseBy, invert) {
+    this.current = current;
+    this.max = max;
+    this.increaseBy = increaseBy != null ? increaseBy : 1;
+    this.invert = invert != null ? invert : false;
+  }
+
+  RotationTransformation.prototype.increase = function(howMuch) {
+    if (howMuch == null) {
+      howMuch = this.increaseBy || 1;
+    }
+    return this.current += howMuch;
+  };
+
+  RotationTransformation.prototype.fullyEnded = function() {
+    return this.ended();
+  };
+
+  RotationTransformation.prototype.ended = function() {
+    if (!this.invert) {
+      return this.current >= this.max;
+    } else {
+      return this.current <= this.max;
+    }
+  };
+
+  RotationTransformation.prototype.increaseIfNotEnded = function() {
+    if (!this.ended()) {
+      return this.increase();
+    }
+  };
+
+  RotationTransformation.prototype.rotate = function() {
+    return MathUtils.toRadians(this.current);
+  };
+
+  return RotationTransformation;
+
+})();
+
+Transformation = (function() {
+  Transformation.TYPES = {
+    Y: 'y',
+    X: 'x'
+  };
+
+  Transformation.INCREASE_TYPES = {
+    X: Transformation.X,
+    Y: Transformation.Y,
+    BOTH: 'both'
+  };
+
+  function Transformation(current, max, increaseBy, invert) {
+    this.current = current != null ? current : {
+      x: 0,
+      y: 0
+    };
+    this.max = max != null ? max : {
+      x: 0,
+      y: 0
+    };
+    this.increaseBy = increaseBy != null ? increaseBy : {
+      x: 1,
+      y: 1
+    };
+    this.invert = invert != null ? invert : {
+      x: false,
+      y: false
+    };
+  }
+
+  Transformation.prototype.increase = function(which, howMuch) {
+    howMuch = this.increaseBy[which] || 1;
+    return this.current[which] += howMuch;
+  };
+
+  Transformation.prototype.fullyEnded = function() {
+    return this.ended(Transformation.TYPES.Y) && this.ended(Transformation.TYPES.X);
+  };
+
+  Transformation.prototype.partlyEnded = function() {
+    return this.ended(Transformation.TYPES.Y) || this.ended(Transformation.TYPES.X);
+  };
+
+  Transformation.prototype.ended = function(which) {
+    if (!this.invert[which]) {
+      return this.current[which] >= this.max[which];
+    } else {
+      return this.current[which] <= this.max[which];
+    }
+  };
+
+  Transformation.prototype.increaseIfNotEnded = function(which) {
+    if (which == null) {
+      which = Transformation.INCREASE_TYPES.BOTH;
+    }
+    switch (which) {
+      case Transformation.INCREASE_TYPES.Y:
+        if (!this.ended(Transformation.TYPES.Y)) {
+          return this.increase(Transformation.TYPES.Y);
+        }
+        break;
+      case Transformation.INCREASE_TYPES.X:
+        if (!this.ended(Transformation.TYPES.X)) {
+          return this.increase(Transformation.TYPES.X);
+        }
+        break;
+      case Transformation.INCREASE_TYPES.BOTH:
+        if (!this.ended(Transformation.TYPES.Y)) {
+          this.increase(Transformation.TYPES.Y);
+        }
+        if (!this.ended(Transformation.TYPES.X)) {
+          return this.increase(Transformation.TYPES.X);
+        }
+    }
+  };
+
+  Transformation.prototype.x = function() {
+    return this.current.x;
+  };
+
+  Transformation.prototype.y = function() {
+    return this.current.y;
+  };
+
+  return Transformation;
+
+})();
+
+Transformations = (function() {
+  function Transformations(t, callbacks) {
+    this.t = t != null ? t : {
+      translate: null,
+      rotate: null,
+      scale: null
+    };
+    if (callbacks == null) {
+      callbacks = {
+        increase: null,
+        ended: null
+      };
+    }
+    this.translate = this.t.translate;
+    this.rotate = this.t.rotate;
+    this.scale = this.t.scale;
+    this.callbacks = {
+      ended: callbacks.ended || function() {
+        return false;
+      },
+      increase: callbacks.increase || function() {
+        return false;
+      }
+    };
+  }
+
+  Transformations.prototype.increase = function() {
+    return this.callbacks.increase();
+  };
+
+  Transformations.prototype.ended = function() {
+    return this.callbacks.ended();
+  };
+
+  return Transformations;
+
+})();
 
 CanvasFractal = (function() {
   CanvasFractal.RECURISE = 'recursive';
@@ -7,7 +175,7 @@ CanvasFractal = (function() {
 
   CanvasFractal.MAXIMUM_STEPS = 7;
 
-  CanvasFractal.DEFAULT_STEP = 1;
+  CanvasFractal.DEFAULT_STEP = 0;
 
   function CanvasFractal() {
     this.canvas = document.getElementById('canvas');
@@ -17,7 +185,6 @@ CanvasFractal = (function() {
     }
     this.initClickListener();
     this.currentStep = CanvasFractal.DEFAULT_STEP;
-    this.firstTime = false;
   }
 
   CanvasFractal.prototype.setupContext = function() {
@@ -27,7 +194,8 @@ CanvasFractal = (function() {
     this.ctx.lineTo(500, 500);
     this.ctx.lineTo(0, 500);
     this.ctx.closePath();
-    return this.ctx.stroke();
+    this.ctx.stroke();
+    return this._savedStep = this.currentStep;
   };
 
   CanvasFractal.prototype.draw = function() {
@@ -38,18 +206,167 @@ CanvasFractal = (function() {
   CanvasFractal.prototype.initClickListener = function() {
     return $(window).on('keydown', (function(_this) {
       return function(ev) {
-        if (ev.which === 32) {
-          _this.increaseCurrentStep();
-          _this.ctx.setTransform(1, 0, 0, 1, 0, 0);
-          _this.ctx.clearRect(0, 0, _this.canvas.width, _this.canvas.height);
-          _this.startDrawing(_this.currentStep);
-          return _this.firstTime = true;
+        var transformations;
+        switch (ev.which) {
+          case 32:
+            _this.increaseCurrentStep();
+            _this.clear();
+            return _this.startDrawing(_this.currentStep);
+          case 49:
+            _this.clear();
+            transformations = new Transformations({
+              rotate: null,
+              translate: new Transformation({
+                x: 0,
+                y: 0
+              }, {
+                x: 250,
+                y: 250
+              }),
+              scale: new Transformation({
+                x: 1,
+                y: 1
+              }, {
+                x: 0.5,
+                y: 0.5
+              }, {
+                x: -0.005,
+                y: -0.005
+              }, {
+                x: true,
+                y: true
+              })
+            }, {
+              increase: function() {
+                transformations.scale.increaseIfNotEnded(Transformation.INCREASE_TYPES.BOTH);
+                return transformations.translate.increaseIfNotEnded(Transformation.INCREASE_TYPES.BOTH);
+              },
+              ended: function() {
+                return transformations.scale.fullyEnded() && transformations.translate.fullyEnded();
+              }
+            });
+            return _this.doAnimation(transformations);
+          case 50:
+            _this.clear();
+            transformations = new Transformations({
+              rotate: new RotationTransformation(0, -90, -1, true),
+              translate: new Transformation({
+                x: 0,
+                y: 0
+              }, {
+                x: 250,
+                y: 0
+              }),
+              scale: new Transformation({
+                x: 1,
+                y: 1
+              }, {
+                x: -0.5,
+                y: 0.5
+              }, {
+                x: -0.005,
+                y: -0.005
+              }, {
+                x: true,
+                y: true
+              })
+            }, {
+              increase: function() {
+                transformations.scale.increaseIfNotEnded(Transformation.INCREASE_TYPES.BOTH);
+                transformations.translate.increaseIfNotEnded(Transformation.INCREASE_TYPES.BOTH);
+                return transformations.rotate.increaseIfNotEnded();
+              },
+              ended: function() {
+                return transformations.scale.fullyEnded() && transformations.translate.fullyEnded() && transformations.rotate.fullyEnded();
+              }
+            });
+            return _this.doAnimation(transformations);
+          case 51:
+            _this.clear();
+            transformations = new Transformations({
+              translate: new Transformation({
+                x: 0,
+                y: 0
+              }, {
+                x: 250,
+                y: 0
+              }),
+              scale: new Transformation({
+                x: 1,
+                y: 1
+              }, {
+                x: -0.5,
+                y: 0.5
+              }, {
+                x: -0.005,
+                y: -0.005
+              }, {
+                x: true,
+                y: true
+              })
+            }, {
+              increase: function() {
+                transformations.scale.increaseIfNotEnded(Transformation.INCREASE_TYPES.BOTH);
+                return transformations.translate.increaseIfNotEnded(Transformation.INCREASE_TYPES.BOTH);
+              },
+              ended: function() {
+                return transformations.scale.fullyEnded() && transformations.translate.fullyEnded();
+              }
+            });
+            return _this.doAnimation(transformations);
+          case 52:
+            _this.clear();
+            transformations = new Transformations({
+              rotate: new RotationTransformation(0, 90, 1),
+              translate: new Transformation({
+                x: 0,
+                y: 0
+              }, {
+                x: 125,
+                y: 250
+              }),
+              scale: new Transformation({
+                x: 1,
+                y: 1
+              }, {
+                x: 0.25,
+                y: 0.25
+              }, {
+                x: -0.005,
+                y: -0.005
+              }, {
+                x: true,
+                y: true
+              })
+            }, {
+              increase: function() {
+                transformations.scale.increaseIfNotEnded(Transformation.INCREASE_TYPES.BOTH);
+                transformations.translate.increaseIfNotEnded(Transformation.INCREASE_TYPES.BOTH);
+                return transformations.rotate.increaseIfNotEnded();
+              },
+              ended: function() {
+                return transformations.scale.fullyEnded() && transformations.translate.fullyEnded() && transformations.rotate.fullyEnded();
+              }
+            });
+            return _this.doAnimation(transformations);
         }
       };
     })(this));
   };
 
-  CanvasFractal.prototype.clear = function() {};
+  CanvasFractal.prototype.clear = function(preserveTransform) {
+    if (preserveTransform == null) {
+      preserveTransform = false;
+    }
+    if (preserveTransform) {
+      this.ctx.save();
+      this.ctx.setTransform(1, 0, 0, 1, 0, 0);
+    }
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    if (preserveTransform) {
+      return this.ctx.restore();
+    }
+  };
 
   CanvasFractal.prototype.increaseCurrentStep = function() {
     if (this.currentStep >= CanvasFractal.MAXIMUM_STEPS) {
@@ -57,6 +374,32 @@ CanvasFractal = (function() {
     } else {
       return this.currentStep++;
     }
+  };
+
+  CanvasFractal.prototype.doAnimation = function(transformations) {
+    var interval;
+    return interval = setInterval((function(_this) {
+      return function() {
+        transformations.increase();
+        if (transformations.ended()) {
+          clearInterval(interval);
+        }
+        _this.clear(false);
+        _this.ctx.save();
+        if (transformations.translate != null) {
+          _this.ctx.translate(transformations.translate.x(), transformations.translate.y());
+        }
+        if (transformations.rotate != null) {
+          _this.ctx.rotate(transformations.rotate.rotate());
+        }
+        if (transformations.scale != null) {
+          _this.ctx.scale(transformations.scale.x(), transformations.scale.y());
+        }
+        _this.ctx.fillStyle = 'red';
+        _this.startDrawing(0, CanvasFractal.ONCE);
+        return _this.ctx.restore();
+      };
+    })(this), 5);
   };
 
   CanvasFractal.prototype.startDrawing = function(currentStep, how) {
@@ -67,6 +410,7 @@ CanvasFractal = (function() {
       case CanvasFractal.RECURSIVE:
         if (currentStep > 0) {
           currentStep--;
+          this.ctx.save();
           this.ctx.save();
           this.ctx.save();
           this.ctx.save();
@@ -90,7 +434,8 @@ CanvasFractal = (function() {
           this.ctx.rotate(MathUtils.toRadians(90));
           this.ctx.scale(0.25, 0.25);
           this.ctx.fillStyle = 'blue';
-          return this.startDrawing(currentStep);
+          this.startDrawing(currentStep);
+          return this.ctx.restore();
         } else {
           return this.startDrawing(0, CanvasFractal.ONCE);
         }
